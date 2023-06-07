32ff4e11f63b8afbf9e51dedcec22dffe7ac7283
kholostyak17 <kholostyak17@gmail.com>
2023-06-07 21:05:06 +0200

    feat: MDEditor integrated & JSON output on textarea

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MDEditor from "@uiw/react-md-editor";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default function App() {
+  const [value, setValue] = React.useState("**Hello world!!!**");
+  return (
+    <div className="container" style={{ margin: "20px" }}>
+      <MDEditor value={value} onChange={setValue} />
+      {/* <MDEditor.Markdown source={value} style={{ whiteSpace: "pre-wrap" }} /> */}
+
+      <textarea
+        value={JSON.stringify(value)}
+        style={{
+          minHeight: "200px",
+          width: "100%",
+          background: "#DEDEDE",
+          marginTop: "20px",
+        }}
+      />
+
+      <p>
+        by{" "}
+        <a href="https://www.github.com/kholostyak17" target="_blank">
+          @kholostyak17
+        </a>
+      </p>
+    </div>
+  );
 }
-
-export default App;
